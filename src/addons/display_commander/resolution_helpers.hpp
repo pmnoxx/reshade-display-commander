56e2e568aeb2aedca22aaf9d4440d1524fe786b2
Piotr Mikulski <piotr.mikulski@gmail.coM>
2025-09-06 21:58:19 -0700

    Remove unused code

--- src/addons/display_commander/resolution_helpers.hpp
+++ src/addons/display_commander/resolution_helpers.hpp
@@ -5,15 +5,6 @@
 namespace resolution {
 
 
-
-
-
-// Helper function to get selected resolution
-bool GetSelectedResolution(int monitor_index, int resolution_index, int& out_width, int& out_height);
-
-// Helper function to get selected refresh rate
-bool GetSelectedRefreshRate(int monitor_index, int width, int height, int refresh_rate_index, float& out_refresh_rate);
-
 // Helper function to get selected refresh rate as rational values
 bool GetSelectedRefreshRateRational(int monitor_index, int width, int height, int refresh_rate_index,
                                    UINT32& out_numerator, UINT32& out_denominator);
