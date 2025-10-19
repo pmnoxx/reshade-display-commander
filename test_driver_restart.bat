2760ff22ce4b48326418c3f46d6292866bd26efa
Piotr Mikulski <piotr.mikulski@gmail.coM>
2025-10-19 07:58:58 -0700

    Delete driver_restart tool

--- test_driver_restart.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Testing driver restart tool...
-echo.
-echo Running with verbose mode...
-"D:\repos\reshade-display-commander\build\tools\driver_restart.exe" /v
-echo.
-echo Exit code: %ERRORLEVEL%
-pause
