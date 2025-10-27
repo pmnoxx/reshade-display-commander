b126f81d37798eed74b4b8f45b9ba7a4379228dd
Piotr Mikulski <piotr.mikulski@gmail.coM>
2025-10-27 06:04:12 -0700

    Cleanup UI

--- src/addons/display_commander/addon.cpp
+++ src/addons/display_commander/addon.cpp
@@ -3,4 +3,4 @@
 // Export addon information
 extern "C" __declspec(dllexport) constexpr const char* NAME = "Display Commander";
 extern "C" __declspec(dllexport) constexpr const char* DESCRIPTION =
-    "RenoDX Display Commander - Advanced display and performance management. Note: Applying window operations from the main thread can crash some apps; this addon performs them on a background thread.";
+    "RenoDX Display Commander - Advanced display and performance management.";
